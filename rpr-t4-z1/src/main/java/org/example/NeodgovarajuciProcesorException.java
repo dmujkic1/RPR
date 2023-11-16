df340d400c4cc06a63303662e305077bee1414e1
dmujkic1 <dmujkic1@etf.unsa.ba>
2023-11-16 20:11:55 +0100

    Add files via upload

---
+++ rpr-t4-z1/src/main/java/org/example/NeodgovarajuciProcesorException.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.*;
+public class NeodgovarajuciProcesorException extends Exception{
+    private String poruka;
+    public
+    NeodgovarajuciProcesorException(String poruka)
+    {
+        this.poruka=poruka;
+    }
+    String getPoruka()
+    {
+        return poruka;
+    }
+}
